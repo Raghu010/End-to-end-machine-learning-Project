aabd7495a3edbacfb9cc92275a94c5bba4a7d093
Raghu <raghuraghav929@gmail.com>
2023-04-12 15:37:24 +0530

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ import dill
 from src.exception import CustomException
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
-from sklearn.model_selection import  GridSearchCV
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -52,5 +52,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
